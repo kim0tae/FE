09924547b6c42d8b693a5fafd97a91d84c71dcbe
Kim youngtae <ytkim@infovine.co.kr>
2024-06-04 18:42:24 +0900

    [UPDATE] 버튼 css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import { styled } from 'styled-components';
 import Home from './routes/home';
+import reset from 'styled-reset';
 
 const router = createBrowserRouter([
   {
@@ -10,8 +12,31 @@ const router = createBrowserRouter([
   },
 ]);
 
+const GlobalStyles = createGlobalStyle` 
+  ${reset};
+  * {
+    box-sizing: border-box;
+    .scroll {
+      overflow: hidden;
+    }
+  }
+  body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+`;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Wrapper>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </Wrapper>
+  );
 }
 
 export default App;
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,37 @@
 import { useState, useEffect } from 'react';
 import { styled } from 'styled-components';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  justfy-content: center;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  height: 30px;
+  width: 200px;
+  border-radius: 5px;
+  border: none;
+  background-color: #056ac9;
+  color: white;
+  font-weight: 500;
+`;
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: 1 px solid;
+  width: 100%;
+  font-size: 16px;
+  &[type='submit'] {
+    background-color: #1d9bf0;
+    color: black;
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
 
 export default function Home() {
   const [data, setData] = useState([]);
@@ -10,5 +40,10 @@ export default function Home() {
       .then((res) => res.json())
       .then((data) => setData(data.page));
   });
-  return <Wrapper>{data}</Wrapper>;
+  return (
+    <Wrapper>
+      <Button>{data}</Button>
+      <Input name="name" placeholder="" type="text" required />
+    </Wrapper>
+  );
 }
