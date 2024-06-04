cec9b3b08f3dbfc4b10ea53a802f719942e70ab3
Kim youngtae <ytkim@infovine.co.kr>
2024-06-04 16:45:14 +0900

    [UPDATE] init - typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8000')
+      .then((res) => res.json())
+      .then((data) => setData(data.page));
+  });
+  return <div className="App">{data}</div>;
+}
+
+export default App;
