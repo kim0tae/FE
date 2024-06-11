b04b04b6efbbbec3bf944bc09c968444405f83e6
Kim youngtae <ytkim@infovine.co.kr>
2024-06-11 18:44:42 +0900

    [FEATUER] 레이어팝업 수정

--- src/components/layer-popup.tsx
+++ src/components/layer-popup.tsx
@@ -7,24 +7,60 @@ const Wrapper = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
 `;
+
 const Dim = styled.div`
   position: fixed;
   background-color: rgba(0, 0, 0, 0.3);
   width: 100%;
   height: 100%;
 `;
+
 const Popup = styled.div`
   height: 300px;
   width: 300px;
-  background-color: beige;
+  background-color: white;
   border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
-const Content = styled.div`
+
+const Content = styled.p`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 100%;
+`;
+
+const Button = styled.button`
+  height: 40px;
+  width: 50%;
+  border-radius: 5px;
+  border: 1px solid #056ac9;
+  background-color: #f0f5fa;
+  color: #1074f3;
+  font-weight: 600;
+  &:nth-child(2) {
+    background-color: #056ac9;
+    color: white;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  padding: 20px;
+`;
+
+const ContentTitle = styled.h1`
+  margin: 15px;
+  font-size: 20px;
+  font-weight: 600;
 `;
 
 export default function LayerPopup() {
@@ -33,7 +69,12 @@ export default function LayerPopup() {
       <Dim />
       <Wrapper>
         <Popup>
-          <Content> Test Content </Content>
+          <ContentTitle> 알림 </ContentTitle>
+          <Content>Test Content</Content>
+          <ButtonContainer>
+            <Button>취소</Button>
+            <Button>확인</Button>
+          </ButtonContainer>
         </Popup>
       </Wrapper>
     </>
