4831736ab64687eb3f0d34250d495147d0e6bed5
유기백 <zxdfg114@gmail.com>
2024-07-04 14:11:26 +0900

    [feat] Login, Upload 토큰 인증 추가

--- src/routes/contents/board.tsx
+++ src/routes/contents/board.tsx
@@ -1,6 +1,6 @@
-import { styled } from 'styled-components';
-import * as S from '../../components/styles/ui-components';
-import React, { useState, useEffect } from 'react';
+import { styled } from "styled-components";
+import * as S from "../../components/styles/ui-components";
+import React, { useState, useEffect } from "react";
 
 const Form = styled.form`
   margin-top: 50px;
@@ -25,60 +25,74 @@ const TextArea = styled.textarea`
 `;
 
 export default function InsertBoard() {
-  const [formData, setFormData] = useState({
-    title: '',
-    contents: '',
-  });
-  const onChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
-    // const { name, value } = e.target;
-    // let formattedValue = value;
-    // if (name === 'title') {
-    //   formattedValue = value;
-    // }
-    setFormData({
-      ...formData,
-      [e.target.name] : e.target.value,
+    const [formData, setFormData] = useState({
+        title: "",
+        contents: "",
     });
-  };
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-	console.log(formData)
-    // const { title, contents } = formData;
-    try {
-      const response = await fetch('http://192.170.1.173:8000/board/upload', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
+    const onChange = (
+        e:
+            | React.ChangeEvent<HTMLInputElement>
+            | React.ChangeEvent<HTMLTextAreaElement>
+    ) => {
+        // const { name, value } = e.target;
+        // let formattedValue = value;
+        // if (name === 'title') {
+        //   formattedValue = value;
+        // }
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(formData);
+        // const { title, contents } = formData;
+        try {
+            const response = await fetch(
+                "http://192.170.1.173:8000/board/upload",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `${localStorage.getItem("token")}`,
+                    },
+                    body: JSON.stringify(formData),
+                }
+            );
 
-      const data = await response.json();
-      if (!response.ok) {
-      } else {
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-      }
-    } finally {
-    }
-  };
-  return (
-    <>
-      <S.ColumnWrapper>
-        <Form onSubmit={onSubmit}>
-          <S.Input
-            onChange={onChange}
-            name="title"
-            placeholder="제목을 입력해 주세요."
-            type="text"
-            value={formData.title}
-            required
-          />
-          <TextArea name="contents" placeholder="게시글을 작성해주세요." onChange={onChange} />
-          <S.Input type="submit" value="게시글 작성" />
-        </Form>
-      </S.ColumnWrapper>
-    </>
-  );
+            const data = await response.json();
+            if (!response.ok) {
+                console.log(data);
+            } else {
+              console.log(data)
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+            }
+        } finally {
+        }
+    };
+    return (
+        <>
+            <S.ColumnWrapper>
+                <Form onSubmit={onSubmit}>
+                    <S.Input
+                        onChange={onChange}
+                        name="title"
+                        placeholder="제목을 입력해 주세요."
+                        type="text"
+                        value={formData.title}
+                        required
+                    />
+                    <TextArea
+                        name="contents"
+                        placeholder="게시글을 작성해주세요."
+                        onChange={onChange}
+                    />
+                    <S.Input type="submit" value="게시글 작성" />
+                </Form>
+            </S.ColumnWrapper>
+        </>
+    );
 }
--- src/routes/user/login.tsx
+++ src/routes/user/login.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { styled } from 'styled-components';
-import { Link, useNavigate } from 'react-router-dom';
-import LayerPopup from '../../components/layer-popup';
+import { useState } from "react";
+import { styled } from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import LayerPopup from "../../components/layer-popup";
 
-import * as S from '../../components/styles/ui-components';
+import * as S from "../../components/styles/ui-components";
 
 const Title = styled.h1`
   font-size: 42px;
@@ -24,98 +24,111 @@ const UnOrderLists = styled.ul`
 `;
 
 const linkStyle = {
-  color: '#888',
-  textDecoration: 'none',
-  padding: '10px',
+    color: "#888",
+    textDecoration: "none",
+    padding: "10px",
 };
 
 const List = styled.li``;
 
 export default function Login() {
-  const navigate = useNavigate();
-  const [isLoading, setLoading] = useState(false);
-  const [id, setID] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { name, value },
-    } = e;
-    if (name === 'id') {
-      setID(value);
-    } else if (name === 'password') {
-      setPassword(value);
-    }
-  };
-  const onConfirm = () => {};
-  const onCancel = () => {};
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (isLoading || id === '' || password === '') return;
-    try {
-      setLoading(true);
-      const response = await fetch('http://192.170.1.173:8000/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id,
-          password,
-        }),
-      });
-      if (!response.ok) {
-        <LayerPopup contentInfo={'Error'} confirm={onConfirm} cancel={onCancel} />;
-      }
-      var data = await response.json();
-      if (data.success) {
-        console.log(data);
-        navigate(`/my-profile/${id}`, { state: { id } }); //2. ``을 통해 id 를 동적으로 전달합니다.
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        setError(error.message);
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
-  return (
-    <>
-      <S.ColumnWrapper>
-        <Title>로그인</Title>
-        <Form onSubmit={onSubmit}>
-          <S.Input onChange={onChange} name="id" placeholder="ID를 입력해 주세요." type="text" value={id} required />
-          <S.Input
-            onChange={onChange}
-            name="password"
-            placeholder="비밀번호를 입력해 주세요."
-            type="password"
-            value={password}
-            required
-          />
-          <S.Input type="submit" value="로그인" />
-        </Form>
-        <S.Wrapper>
-          <UnOrderLists>
-            <List>
-              <Link style={linkStyle} target="_blank" to="/find-id">
-                아이디 찾기
-              </Link>
-            </List>
-            <List>
-              <Link style={linkStyle} target="_blank" to="/find-pwd">
-                비밀번호 찾기
-              </Link>
-            </List>
-            <List>
-              <Link style={linkStyle} target="_blank" to="/join">
-                회원가입
-              </Link>
-            </List>
-          </UnOrderLists>
-        </S.Wrapper>
-      </S.ColumnWrapper>
-    </>
-  );
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+    const [id, setID] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {
+            target: { name, value },
+        } = e;
+        if (name === "id") {
+            setID(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+    const onConfirm = () => {};
+    const onCancel = () => {};
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (isLoading || id === "" || password === "") return;
+        try {
+            setLoading(true);
+            const response = await fetch("http://192.170.1.173:8000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    id,
+                    password,
+                }),
+            });
+            const data = await response.json();
+            if (data.success) {
+                localStorage.setItem("token", data.jwtToken);
+                console.log(data);
+                navigate(`/my-profile/${id}`, { state: { id } }); //2. ``을 통해 id 를 동적으로 전달합니다.
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                setError(error.message);
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+    return (
+        <>
+            <S.ColumnWrapper>
+                <Title>로그인</Title>
+                <Form onSubmit={onSubmit}>
+                    <S.Input
+                        onChange={onChange}
+                        name="id"
+                        placeholder="ID를 입력해 주세요."
+                        type="text"
+                        value={id}
+                        required
+                    />
+                    <S.Input
+                        onChange={onChange}
+                        name="password"
+                        placeholder="비밀번호를 입력해 주세요."
+                        type="password"
+                        value={password}
+                        required
+                    />
+                    <S.Input type="submit" value="로그인" />
+                </Form>
+                <S.Wrapper>
+                    <UnOrderLists>
+                        <List>
+                            <Link
+                                style={linkStyle}
+                                target="_blank"
+                                to="/find-id"
+                            >
+                                아이디 찾기
+                            </Link>
+                        </List>
+                        <List>
+                            <Link
+                                style={linkStyle}
+                                target="_blank"
+                                to="/find-pwd"
+                            >
+                                비밀번호 찾기
+                            </Link>
+                        </List>
+                        <List>
+                            <Link style={linkStyle} target="_blank" to="/join">
+                                회원가입
+                            </Link>
+                        </List>
+                    </UnOrderLists>
+                </S.Wrapper>
+            </S.ColumnWrapper>
+        </>
+    );
 }
